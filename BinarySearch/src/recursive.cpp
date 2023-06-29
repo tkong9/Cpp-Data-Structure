a94212b698bd6dc7c7190f666d5a1823bb20908d
Taewon Kong <taewon2711@gmail.com>
2023-06-29 20:39:12 +0900

    binary search update

--- BinarySearch/src/recursive.cpp
+++ BinarySearch/src/recursive.cpp
@@ -3,56 +3,58 @@
 #include <cassert>
 
 /**
- * Binary search algorithm to find the index of a target element in a sorted vector.
+ * A recursive implementation of binary search.
  *
- * @param arr The sorted vector to search in.
- * @param target The element to search for.
- * @return The index of the target element if found, -1 otherwise.
+ * Binary search is an efficient algorithm for finding an item from a sorted list of items.
+ * It works by repeatedly dividing in half the portion of the list that could contain the item,
+ * until you've narrowed down the possible locations to just one.
+ *
+ * @param arr The sorted array to be searched.
+ * @param low The low index of the portion of the array to be searched.
+ * @param high The high index of the portion of the array to be searched.
+ * @param target The value being searched for.
+ * @return Returns the index of target in arr if found, else returns -1.
  */
-int binarySearch(const std::vector<int>& arr, int target) {
+int binary_search(const std::vector<int>& arr, int low, int high, int target) {
     // TODO: Implement the binary search algorithm recursively here
 
-    return -1;  // Target element not found in the vector
+    return -1;
 }
 
-void testBinarySearch() {
+
+void test_binary_search() {
+    std::cout << "Running binary_search tests...";
     // Test case 1: Positive numbers
     std::vector<int> numbers1 = {2, 5, 8, 12, 16, 20, 30};
-    assert(binarySearch(numbers1, 12) == 3);
-
+    assert(binary_search(numbers1, 0, numbers1.size() - 1, 12) == 3);
     // Test case 2: Negative numbers
     std::vector<int> numbers2 = {-30, -16, -3, 0, 5, 8, 12};
-    assert(binarySearch(numbers2, -16) == 1);
-
+    assert(binary_search(numbers2, 0, numbers2.size() - 1,-16) == 1);
     // Test case 3: Zero and positive number
     std::vector<int> numbers3 = {0, 5, 8, 12, 16, 20, 30};
-    assert(binarySearch(numbers3, 0) == 0);
-
+    assert(binary_search(numbers3, 0, numbers3.size() -1, 0) == 0);
     // Test case 4: Zero and negative number
     std::vector<int> numbers4 = {-30, -16, -3, 0, 5, 8, 12};
-    assert(binarySearch(numbers4, 0) == 3);
-
+    assert(binary_search(numbers4,0, numbers4.size() - 1, 0) == 3);
     // Test case 5: Empty vector
     std::vector<int> numbers5 = {};
-    assert(binarySearch(numbers5, 8) == -1);
-
+    assert(binary_search(numbers5, 0, numbers5.size() - 1, 8) == -1);
     // Test case 6: Target not found
     std::vector<int> numbers6 = {2, 5, 8, 12, 16, 20, 30};
-    assert(binarySearch(numbers6, 9) == -1);
-
+    assert(binary_search(numbers6, 0, numbers6.size() - 1, 9) == -1);
     // Test case 7: Target at the beginning
     std::vector<int> numbers7 = {2, 5, 8, 12, 16, 20, 30};
-    assert(binarySearch(numbers7, 2) == 0);
-
+    assert(binary_search(numbers7, 0, numbers7.size() - 1, 2) == 0);
     // Test case 8: Target at the end
     std::vector<int> numbers8 = {2, 5, 8, 12, 16, 20, 30};
-    assert(binarySearch(numbers8, 30) == 6);
+    assert(binary_search(numbers8, 0, numbers8.size() - 1, 30) == 6);
+    std::cout << "All tests passed!" << std::endl;
 }
 
 int main() {
     // Run the tests to ensure everything works
-    std::cout << "Running tests..." << std::endl;
-    testBinarySearch();
+    std::cout << "Running tests...";
+    test_binary_search();
     std::cout << "All tests passed." << std::endl;
 
     return 0;
